343f0ca29df4bc1939ec0b932c136fef3c550cb5
Fabian Steeg <fsteeg@gmail.com>
2009-05-28 23:56:31 +0000

    Incomplete - # 10: [qlog4eclipse] Use new server: moved classes from qlog.swing used in qlog.plugin into its own component qlog.ui (so that qlog.plugin does not depend on qlog.swing) http://my-trac.assembla.com/quui/ticket/10

---
+++ com.quui.qlog.ui/src/com/quui/qlog/ui/Filter.java
@@ -0,0 +1,37 @@
+package com.quui.qlog.ui;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.regex.Pattern;
+
+public class Filter
+{
+	public static String find(String f, TableBuilder tb)
+	{
+		if (f.equals(""))
+		{
+			return tb.getContent();
+		}
+
+		String line;
+		String matches = "";
+		Pattern pattern = Pattern.compile(f);
+		BufferedReader r = new BufferedReader(new StringReader(tb.getContent()));
+		try
+		{
+			while ((line = r.readLine()) != null)
+			{
+				if (pattern.matcher(line).find())
+				{
+					matches += line;
+				}
+			}
+		}
+		catch (IOException e1)
+		{
+			e1.printStackTrace();
+		}
+		return tb.getCss() + matches;
+	}
+}
